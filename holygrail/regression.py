200c233c8812ac7062983fb5b4be1868d92d09c5
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-12-07 23:32:40 +0000

    Interim check-in towards torsion-angle regression.

--- holygrail/regression.py
+++ holygrail/regression.py
@@ -8,71 +8,70 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 @author:  neilswainston
 '''
 import itertools
-import random
+import sys
+import climate
 
-import holygrail
 from synbiochem.utils import structure_utils as struct_utils
+import holygrail
+import synbiochem.ann
 
 
-def get_regression_data(max_ids, num_samples, nmer_len):
-    '''Gets random data for regression analyses.'''
-    for pdb_id in struct_utils.get_pdb_ids(max_ids):
-        try:
-            for data in _sample_regression_data(pdb_id, num_samples,
-                                                nmer_len):
-                print '\t'.join([str(datum) for datum in data])
-        except AssertionError, err:
-            print err
+def regress(sample_size, struct_pattern, split, hidden_layers):
+    '''Regression of phi/psi angles of peptides, specified by structure
+    pattern as regexps.'''
+    climate.enable_default_logging()
+
+    # Get random peptides that match structure patterns from PDB:
+    pdb_data = holygrail.get_pdb_data(sample_size, [struct_pattern], True)
+
+    # Convert peptides to inputs, based on amino acid properties:
+    x_data = holygrail.get_input_data([i[0] for v in pdb_data.values()
+                                       for i in v])
+
+    # Convert classess to outputs (these are based on structure patterns):
+    y_data = [_get_phi_psi_data(v[2][0], v[2][1], v[3])
+              for sublist in pdb_data.values()
+              for v in sublist]
+
+    # Randomise input and output data order:
+    x_data, y_data = synbiochem.ann.randomise_order(x_data, y_data)
+
+    # Split data into training and classifying:
+    ind = int(split * len(x_data))
+
+    # Perform regression:
+    regressor = synbiochem.ann.Regressor()
+    regressor.train(x_data[:ind], y_data[:ind], hidden_layers=[hidden_layers])
+
+    y_pred = regressor.predict(x_data[ind:])
+
+    return y_data[ind:], y_pred
 
 
-def _sample_regression_data(pdb_id, num_samples, nmer_len):
-    '''Samples learning data for deep learning.'''
-    pdb_id, all_seqs, inpt, prox_output, phi_psi_output = \
-        _get_regression_data(pdb_id)
-
-    results = []
-
-    num_aa_props = len(holygrail.AA_PROPS['A'])
-
-    for _ in range(num_samples):
-        chn = int(random.random() * len(all_seqs))
-        start = int(random.random() * (len(all_seqs[chn]) - nmer_len))
-        rnge = range(start, start + nmer_len)
-        result = [pdb_id,
-                  ''.join([all_seqs[chn][i] for i in rnge]),
-                  inpt[chn][start * num_aa_props:(start + nmer_len) *
-                            num_aa_props],
-                  [prox_output[chn][i]
-                   for i in _get_sub_square_matrix(start, nmer_len,
-                                                   len(all_seqs[chn]))],
-                  phi_psi_output[chn][start * 2:(start + nmer_len) * 2]]
-
-        results.append(result)
-
-    return results
-
-
-def _get_regression_data(pdb_id):
-    '''Gets input/output for deep learning.'''
+def _get_proximity_data(pdb_id):
+    '''Gets proximity_data for deep learning.'''
     all_sequences = struct_utils.get_sequences(pdb_id)
-    all_proximities = struct_utils.calc_proximities(pdb_id)
-    all_phi_psi_data = struct_utils.get_phi_psi_data(pdb_id)
+    all_proximity_data = struct_utils.calc_proximities(pdb_id)
 
     # Ensure data consistency in terms of data lengths:
-    assert len(all_sequences) == len(all_proximities) == \
-        len(all_phi_psi_data)
-    assert all([len(all_sequences[idx]) == len(all_proximities[idx]) ==
-                len(all_phi_psi_data[idx])
+    assert len(all_sequences) == len(all_proximity_data)
+    assert all([len(all_sequences[idx]) == len(all_proximity_data[idx])
                 for idx in range(len(all_sequences))])
 
     prox_output = [list(itertools.chain.from_iterable(proximities))
-                   for proximities in all_proximities]
+                   for proximities in all_proximity_data]
+
+    return all_sequences, prox_output
+
+
+def _get_phi_psi_data(pdb_id, chain, subrange):
+    '''Gets input/output for deep learning.'''
+    all_phi_psi_data = struct_utils.get_phi_psi_data(pdb_id, chain)
 
     phi_psi_output = [list(itertools.chain.from_iterable(lst))
                       for lst in all_phi_psi_data]
 
-    return pdb_id, all_sequences, holygrail.get_input_data(all_sequences), \
-        prox_output, phi_psi_output
+    return phi_psi_output[0][slice(*[x * 2 for x in subrange])]
 
 
 def _get_sub_square_matrix(idx, lngth, size):
@@ -80,3 +79,18 @@ def _get_sub_square_matrix(idx, lngth, size):
     size.'''
     return [((idx + r) * size) + idx + c for r in range(lngth)
             for c in range(lngth)]
+
+
+def main(argv):
+    '''main method.'''
+    regression = regress(int(argv[1]),
+                         argv[4],
+                         float(argv[2]),
+                         int(argv[3]))
+
+    for output in regression:
+        print output
+
+
+if __name__ == '__main__':
+    main(sys.argv)
